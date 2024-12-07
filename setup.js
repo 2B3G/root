8187219b198a8e31c30ae34f261bdfa7e468b008
2B3G <129078002+2B3G@users.noreply.github.com>
2024-12-07 19:56:27 +0100

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,7 +1,7 @@
 const { spawn } = require('child_process');
 
 const command = 'bash';
-const args = ['root/noninteractive.sh'];
+const args = ['root/root.sh'];
 
 // Spawn the process
 const process = spawn(command, args, { stdio: 'pipe' }); // Use 'pipe' to enable interaction with stdin
@@ -12,8 +12,8 @@ process.stdout.on('data', (data) => {
 
     // When entered root
     if(data.toString().includes("Mission")){
-        process.stdin.write('apt update -y\n');
-        process.stdin.write('apt install curl -y\n');
+        process.stdin.write('apk update -y\n');
+        process.stdin.write('apk get curl -y\n');
         process.stdin.write('curl -sSf https://sshx.io/get | sh\n');
     }
 });
