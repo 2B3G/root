479ec1e4e50e2ddf65fa01f35215291bc7130b48
2B3G <129078002+2B3G@users.noreply.github.com>
2024-12-07 20:10:53 +0100

    Update setup.js

--- setup.js
+++ setup.js
@@ -12,8 +12,8 @@ process.stdout.on('data', (data) => {
 
     // When entered root
     if(data.toString().includes("Mission")){
-        process.stdin.write('apk update -y\n');
-        process.stdin.write('apk get curl -y\n');
+        process.stdin.write('apk update\n');
+        process.stdin.write('apk get curl\n');
         process.stdin.write('curl -sSf https://sshx.io/get | sh\n');
     }
 });
